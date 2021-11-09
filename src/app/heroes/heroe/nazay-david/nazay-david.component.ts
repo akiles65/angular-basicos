26057a42d51ef70b7441f87f7ea5f0f548531514
Luis <luis.villegaas.1993@gmail.com>
2021-11-09 15:41:11 -0400

    primer commit

---
+++ src/app/heroes/heroe/nazay-david/nazay-david.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nazay-david',
+  templateUrl: './nazay-david.component.html'
+})
+export class NazayDavidComponent {
+
+  constructor() { }
+
+}
